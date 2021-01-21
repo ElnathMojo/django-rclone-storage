95f22f849435b8eb2072370817b4a00dbdc12202
ElnathMojo <ElnathMojo@gmail.com>
2021-01-21 03:11:37 -0800

    modified version

--- rclonestorage/rclone.py
+++ rclonestorage/rclone.py
@@ -1,40 +1,49 @@
-"""
-A Python wrapper for rclone.
-"""
-#
-# Licensed to the Apache Software Foundation (ASF) under one or more
-# contributor license agreements.  See the NOTICE file distributed with
-# this work for additional information regarding copyright ownership.
-# The ASF licenses this file to You under the Apache License, Version 2.0
-# (the "License"); you may not use this file except in compliance with
-# the License.  You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
-# pylint: disable=W0102,W0703,C0103
-
-import logging
+import json
+import os
 import subprocess
-import tempfile
+from signal import Signals
 
 
-class RClone:
-    """
-    Wrapper class for rclone.
-    """
+class RcloneException(subprocess.SubprocessError):
+    def __init__(self, returncode, stderr=None):
+        self.returncode = returncode
+        self.stderr = stderr
 
-    def __init__(self, cfg):
-        self.cfg = cfg.replace("\\n", "\n")
-        self.log = logging.getLogger("RClone")
+    def __str__(self):
+        if self.returncode and self.returncode < 0:
+            try:
+                return "RClone died with %r. \n%s" % (
+                    Signals(value=-self.returncode), self.stderr)
+            except ValueError:
+                return "RClone died with unknown signal %d.  \n%s" % (
+                    -self.returncode, self.stderr)
+        else:
+            return "RClone returned non-zero exit status %d. \n%s" % (
+                self.returncode, self.stderr)
 
-    def _execute(self, command_with_args):
+
+class RcloneRemote(object):
+
+    def __init__(self, remote, config_path=None):
+        """
+        Args:
+        remote (string): The name of the rclone remote
+        config_path (string): The path of the rclone config file.
+                              If it's None, rclone should use the default config file.
+        """
+        self.config_path = config_path
+        self.remote = remote
+
+    @staticmethod
+    def _raise_exception(command_result):
+        if command_result["code"] != 0:
+            raise RcloneException(command_result["code"], command_result["error"])
+
+    def _remote_prefix(self, path):
+        return self.remote + ":" + path
+
+    @staticmethod
+    def _execute(command_with_args):
         """
         Execute the given `command_with_args` using Popen
         Args:
@@ -42,7 +51,6 @@ class RClone:
                                          and its arguments. Each argument is given
                                          as a new element in the list.
         """
-        self.log.debug("Invoking : %s", " ".join(command_with_args))
         try:
             with subprocess.Popen(
                     command_with_args,
@@ -50,107 +58,126 @@ class RClone:
                     stderr=subprocess.PIPE) as proc:
                 (out, err) = proc.communicate()
 
-                #out = proc.stdout.read()
-                #err = proc.stderr.read()
-
-                self.log.debug(out)
-                if err:
-                    self.log.warning(err.decode("utf-8").replace("\\n", "\n"))
-
                 return {
                     "code": proc.returncode,
                     "out": out,
                     "error": err
                 }
         except FileNotFoundError as not_found_e:
-            self.log.error("Executable not found. %s", not_found_e)
             return {
                 "code": -20,
                 "error": not_found_e
             }
         except Exception as generic_e:
-            self.log.exception("Error running command. Reason: %s", generic_e)
             return {
                 "code": -30,
                 "error": generic_e
             }
 
-    def run_cmd(self, command, extra_args=[]):
+    def run_cmd(self, command, extra_args=None):
         """
         Execute rclone command
         Args:
             - command (string): the rclone command to execute.
             - extra_args (list): extra arguments to be passed to the rclone command
         """
-        # save the configuration in a temporary file
-        with tempfile.NamedTemporaryFile(mode='wt', delete=True) as cfg_file:
-            # cfg_file is automatically cleaned up by python
-            self.log.debug("rclone config: ~%s~", self.cfg)
-            cfg_file.write(self.cfg)
-            cfg_file.flush()
+        if extra_args is None:
+            extra_args = []
+        command_with_args = ["rclone", command]
+        if self.config_path:
+            command_with_args.extend(["--config", self.config_path])
+        command_with_args += extra_args
+        command_result = self._execute(command_with_args)
+        self._raise_exception(command_result)
+        return command_result
 
-            command_with_args = ["rclone", command, "--config", cfg_file.name]
-            command_with_args += extra_args
-            command_result = self._execute(command_with_args)
-            cfg_file.close()
-            return command_result
-
-    def copy(self, source, dest, flags=[]):
+    def meta(self, path):
         """
-        Executes: rclone copy source:path dest:path [flags]
         Args:
-        - source (string): A string "source:path"
-        - dest (string): A string "dest:path"
+        - path (string): A remote path
+        """
+        head, tail = os.path.split(path)
+        return_values = self.ls(path)
+        if len(return_values) == 1:
+            file_info = return_values[0]
+            if file_info["Name"] == tail or file_info["Path"] == tail:
+                return file_info
+            raise RcloneException(-1, "file not found")
+
+        return_values = self.ls(head)
+        for meta in return_values:
+            if (meta["Name"] == tail or meta["Path"] == tail) and meta["IsDir"]:
+                return meta
+        raise RcloneException(-1, "directory not found")
+
+    def exists(self, path):
+        """
+        Args:
+        - path (string): A remote path
+        """
+        try:
+            self.meta(path)
+            return True
+        except RcloneException as e:
+            if "directory not found" in str(e) or "file not found" in str(e):
+                return False
+            raise e
+
+    def size(self, path):
+        file_size = self.meta(path)["Size"]
+        if file_size < 0:
+            raise RcloneException(-1, "not a valid file")
+        return file_size
+
+    def send_file(self, local_path, remote_path, flags=None):
+        """
+        Executes: rclone copy local_path {remote}:dirname(remote_path) [flags]
+        Args:
+        - local_path (string): A path of a local file
+        - remote_path (string): A path indicates where the remote file will be saved
         - flags (list): Extra flags as per `rclone copy --help` flags.
         """
-        return self.run_cmd(command="copy", extra_args=[source] + [dest] + flags)
+        if flags is None:
+            flags = []
+        if not os.path.isfile(local_path):
+            raise RcloneException(-1, "send_file only accepts a local file.")
+        return self.run_cmd(command="copy",
+                            extra_args=[local_path] + [
+                                self._remote_prefix(os.path.dirname(remote_path))] + flags)
 
-    def sync(self, source, dest, flags=[]):
+    def get_file(self, remote_path, local_path, flags=None):
         """
-        Executes: rclone sync source:path dest:path [flags]
+        Executes: rclone copy {remote}:dirname(remote_path) local_path [flags]
         Args:
-        - source (string): A string "source:path"
-        - dest (string): A string "dest:path"
-        - flags (list): Extra flags as per `rclone sync --help` flags.
+        - remote_path : A path of a remote file
+        - local_path: A path indicates where the local file will be saved
+        - flags (list): Extra flags as per `rclone copy --help` flags.
         """
-        return self.run_cmd(command="sync", extra_args=[source] + [dest] + flags)
+        if flags is None:
+            flags = []
+        if self.meta(remote_path)["IsDir"]:
+            raise RcloneException(-1, "get_file does not accept remote directory.")
+        if not os.path.isdir(os.path.dirname(local_path)):
+            raise RcloneException(-1, "get_file only accepts a valid local path.")
+        return self.run_cmd(command="copy", extra_args=[self._remote_prefix(remote_path)] + [
+            os.path.dirname(local_path)] + flags)
 
-    def listremotes(self, flags=[]):
+    def ls(self, remote_path, flags=None):
         """
-        Executes: rclone listremotes [flags]
+        Executes: rclone lsjson {remote}:path [flags]
         Args:
-        - flags (list): Extra flags as per `rclone listremotes --help` flags.
+        - dest (string): A string representing the location to list.
         """
-        return self.run_cmd(command="listremotes", extra_args=flags)
+        if flags is None:
+            flags = []
+        return json.loads(self.run_cmd(command="lsjson", extra_args=[self._remote_prefix(remote_path)] + flags)["out"])
 
-    def ls(self, dest, flags=[]):
+    def delete(self, path, flags=None):
         """
-        Executes: rclone ls remote:path [flags]
+        Executes: rclone delete {remote}:path
         Args:
-        - dest (string): A string "remote:path" representing the location to list.
+        - dest (string): A string representing the location to delete.
         """
-        return self.run_cmd(command="ls", extra_args=[dest] + flags)
-
-    def lsjson(self, dest, flags=[]):
-        """
-        Executes: rclone lsjson remote:path [flags]
-        Args:
-        - dest (string): A string "remote:path" representing the location to list.
-        """
-        return self.run_cmd(command="lsjson", extra_args=[dest] + flags)
-
-    def delete(self, dest, flags=[]):
-        """
-        Executes: rclone delete remote:path
-        Args:
-        - dest (string): A string "remote:path" representing the location to delete.
-        """
-        return self.run_cmd(command="delete", extra_args=[dest] + flags)
-
-
-def with_config(cfg):
-    """
-    Configure a new RClone instance.
-    """
-    inst = RClone(cfg=cfg)
-    return inst
+        if flags is None:
+            flags = []
+        self.run_cmd(command="delete", extra_args=[self._remote_prefix(path)] + flags)
